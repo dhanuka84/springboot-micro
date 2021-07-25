94a2799ef7549d2f4d777e0e02c441c8c16c559e
dhanuka84 <dhanuka.priyanath@gmail.com>
2021-07-25 19:17:31 +0800

    spring-boot first commit

---
+++ springboot-micro-backend/src/test/java/org/reactor/k8s/micro/AppTest.java
@@ -0,0 +1,12 @@
+package org.reactor.k8s.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
